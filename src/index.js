abfe4ceadb182f5238a2eba58648e517b1073193
phillmac <phillmac@users.noreply.github.com>
2019-11-25 09:03:11 +1100

    Export SessionManager

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const PeerManager = require('./PeerManager.js')
 const DBManager = require('./DBManager.js')
+const SessionManager = require('./SessionManager.js')
+
 
 module.exports = {
   PeerManager,
-  DBManager
+  DBManager,
+  SessionManager
 }
