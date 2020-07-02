8c927a2f842682d4df3c57853394de4cf0cb0376
phillmac <phillmac@users.noreply.github.com>
2020-07-02 10:36:02 +1000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const PeerManager = require('./PeerManager.js')
 const DBManager = require('./DBManager.js')
 const SessionManager = require('./SessionManager.js')
+const getManagers = require('./getManagers.js)
 
 module.exports = {
   PeerManager,
   DBManager,
-  SessionManager
+  SessionManager,
+  getManagers
 }
